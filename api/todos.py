86c53faa76cbe525427f0048b0214ba00dd94386
Sander Camp <sander.camp@kabisa.nl>
2023-07-03 14:41:23 +0200

    json response

--- api/todos.py
+++ api/todos.py
@@ -6,7 +6,7 @@ class handler(BaseHTTPRequestHandler):
         todos = ["Eat breakfast", "Go to work", "Work", "Go home", "Eat dinner", "Sleep"]
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(str(todos).encode())
         return
