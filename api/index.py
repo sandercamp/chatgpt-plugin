c7af211b7ccb888082c03fab9a464ef5944f0a9c
Sander Camp <sander.camp@kabisa.nl>
2023-07-03 13:58:56 +0200

    Json todos

--- api/index.py
+++ api/index.py
@@ -6,5 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+
         self.wfile.write(str('Hello World!!').encode())
         return
--- api/todos.py
+++ api/todos.py
@@ -1,10 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 
+import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        todos = ["Eat breakfast", "Go to work", "Work", "Go home", "Eat dinner", "Sleep"]
+
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/json')
         self.end_headers()
-        self.wfile.write(str('Hello Todos!!').encode())
+        self.wfile.write(json.dumps(todos))
         return
